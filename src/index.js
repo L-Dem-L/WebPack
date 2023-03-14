6c91a606c52702119b7037edfe5a009d8f718132
Demian Rymar <ua.demyan@gmail.com>
2023-03-14 18:06:11 +0200

    Final

--- src/index.js
+++ src/index.js
@@ -7,16 +7,6 @@ import { calcSumNumbers, calcSubtractNumbers } from "./scripts/modules/calc";
 import { Elements } from "./scripts/modules/elements";
 
 // Import methods
-import { includeHTML } from "./scripts/methods/_includeComponents";
+import { includeHTML } from "./scripts/utils/_includeComponents";
 
 // Bisiness logic
-document.addEventListener('DOMContentLoaded', () => {
-  includeHTML();
-  console.log(calcSubtractNumbers(10, 5));
-  console.log(calcSumNumbers(10, 5));
-  const elements = new Elements();
-  elements.addElement('element 1');
-  elements.addElement('element 2');
-  elements.addElement('element 3');
-  console.log(elements);
-});
